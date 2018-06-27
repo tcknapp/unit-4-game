3cc6e11de03fc0052b10590c4b7c2d78aa797e73
tcknapp <trevor.knapp.1991@gmail.com>
2018-06-27 16:29:10 -0700

    almostfinished

---
+++ assets/javascript/game.js
@@ -0,0 +1,137 @@
+
+// players score
+var userTotal = 0;
+
+// wins/losses
+var wins= 0;
+var losses= 0;
+
+// random number for jewel (19-120)
+var randomNumber= Math.floor((Math.random()* 100) +19);
+
+// random value for crystals (1-12)
+var purpleCrystal= Math.floor((Math.random()*12) + 1);
+var greenCrystal= Math.floor((Math.random()*12) + 1);
+var whiteCrystal= Math.floor((Math.random()*12) + 1);
+var blueCrystal= Math.floor((Math.random()*12) + 1);
+
+
+// display wins/losses/total score
+$("#winCount").text(wins);
+$("#lossCount").text(losses);
+$("scoreSpace").text(userTotal);
+
+// displays random number
+$("#randomSpace").text(userTotal);
+
+// for game reset
+function reset(){
+    randomNumber=Math.floor(Math.random()*100)+19;
+    $('#randomSpace').text(randomNumber);
+    purpleCrystal= Math.floor((Math.random()*12) +1);
+    greenCrystal= Math.floor((Math.random()*12) +1);
+    whiteCrystal= Math.floor((Math.random()*12) +1);
+    blueCrystal= Math.floor((Math.random()*12) +1);
+    userTotal= 0;
+    $('#scoreSpace').text(userTotal);
+    }
+
+// function for win/losses
+function winning(){
+    wins++; 
+    $('#winCount').text(wins);
+    reset();
+    }
+
+    function losing(){
+    losses++;
+    $('#lossCount').text(losses);
+    reset()
+     }
+
+// crystal clicks
+
+$('#purple').on('click', function() {
+    userTotal += purpleCrystal;
+    console.log("New total= " + userTotal);
+    $('#scoreSpace').text(userTotal); 
+    if (userTotal > randomNumber) {
+            losses++;
+            $('#lossCount').text(losses);
+            alert("Sorry, try again");
+            console.log("you lost");
+            reset();
+        }
+    if (userTotal == randomNumber) {
+            wins++;
+            $('#winCount').text(wins);
+            console.log("you won");
+            alert("You won!");
+            reset();
+        }
+
+
+$('#green').on('click', function() {
+    userTotal += greenCrystal;
+    console.log("New total= " + userTotal);
+    $('#scoreSpace').text(userTotal); 
+    if (userTotal > randomNumber) {
+            losses++;
+            $('#lossCount').text(losses);
+            alert("Sorry, try again");
+            console.log("you lost");
+            reset();
+        }
+    if (userTotal == randomNumber) {
+            wins++;
+            $('#winCount').text(wins);
+            console.log("you won");
+            alert("You won!");
+            reset();
+        }
+
+});
+
+    $('#white').on('click', function() {
+        userTotal += whiteCrystal;
+        console.log("New total= " + userTotal);
+        $('#scoreSpace').text(userTotal); 
+        if (userTotal > randomNumber) {
+                losses++;
+                $('#lossCount').text(losses);
+                alert("Sorry, try again");
+                console.log("you lost");
+                reset();
+            }
+        if (userTotal == randomNumber) {
+                wins++;
+                $('#winCount').text(wins);
+                console.log("you won");
+                alert("You won!");
+                reset();
+            }
+
+});
+
+    $('#blue').on('click', function() {
+        userTotal += blueCrystal;
+        console.log("New total= " + userTotal);
+        $('#scoreSpace').text(userTotal); 
+        if (userTotal > randomNumber) {
+                losses++;
+                $('#lossCount').text(losses);
+                alert("Sorry, try again");
+                console.log("you lost");
+                reset();
+            }
+        if (userTotal == randomNumber) {
+                wins++;
+                $('#winCount').text(wins);
+                console.log("you won");
+                alert("You won!");
+                reset();
+            }
+
+});
+
+}); 
